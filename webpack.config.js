0662fd0601677e9c6c7d622f5fd518f4a3783f73
品君林 <jinglelin82@gmail.com>
2020-11-18 16:03:37 +0900

    scss 作用設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        // 新增 loader (第三步)
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
