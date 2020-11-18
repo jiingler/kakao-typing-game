b3ad3d4b0bea2efbb469fa184e95cae7c0f7cc74
品君林 <jinglelin82@gmail.com>
2020-11-18 23:50:44 +0900

    功能完成

--- src/index.js
+++ src/index.js
@@ -7,58 +7,81 @@ const scoreSpan = document.querySelector('#score');
 const questionLabel = document.querySelector('#question-label');
 const answerInput = document.querySelector('#answer-input');
 
+const startContainer = document.querySelector('#start');
+const finishContainer = document.querySelector('#finish');
+
+const finishScore = document.querySelector('#finish-score');
+const averageTime = document.querySelector('#average-time');
+
 let isPlayingGame = false;
 let jsonContent = [];
 let nowQuestion = '';
 let nowSecond = 0;
 let countdown;
+let nowScore;
+let totalTime = 0;
+let totalCorrect = 0;
+let aWordUseTime = 0;
+
+window.onload = (()=> {
+  finishContainer.style.display = 'none';
+  startBtn.disabled = true;
+})
 
 
-startBtn.addEventListener('click', () => {
+startBtn.addEventListener('click', (event) => {
+  event.preventDefault()
   isPlayingGame = !isPlayingGame;
-  startBtn.innerHTML = !isPlayingGame? '시작' : '초기화';
+  startBtn.innerHTML = !isPlayingGame ? '시작' : '초기화';
   console.log('isPlayingGame:', isPlayingGame, _.random(0, jsonContent.length - 1));
 
   if (isPlayingGame) {
     nextQuestion()
   } else {
     clearInterval(countdown);
+    resetGame();
   }
 });
+
 
 /**
  * 下一題、時間重設
  */
 function nextQuestion() {
+  clearInterval(countdown);
   if (jsonContent.length === 0) {
-    // end
-    // 到中止頁
-    window.location.replace('./finish.html');
+    startContainer.style.display = 'none';
+    finishContainer.style.display = 'block';
+    finishScore.innerHTML = nowScore;
+    averageTime.innerHTML = totalCorrect === 0 ? 0 : Math.round(totalTime/totalCorrect*10)/10;  //FIXME
+    // reset
+    resetGame();
+
     return;
   }
 
   // 下一題
-  let randomNum =  _.random(0, jsonContent.length - 1)
+  let randomNum = _.random(0, jsonContent.length - 1)
   nowQuestion = jsonContent[randomNum].text;
   nowSecond = jsonContent[randomNum].second;
-
-  console.log(jsonContent[randomNum], nowSecond, nowQuestion)
+  
   // 移除出過的題目
   jsonContent.splice(randomNum, 1);
 
   // 時間重設
   secondSpan.innerHTML = nowSecond;
-  clearInterval(countdown);
+  aWordUseTime = 0;
   countdown = setInterval(() => {
     if (nowSecond > 0) {
       nowSecond--;
       secondSpan.innerHTML = nowSecond;
+      aWordUseTime++;
       if (nowSecond === 0) {
-        nextQuestion();
-        // 總分扣一分
         scoreSpan.innerHTML = Number(scoreSpan.innerHTML - 1);
+        nowScore = Number(scoreSpan.innerHTML);
+        nextQuestion();
       }
-    }
+    }    
   }, 1000)
   questionLabel.innerHTML = nowQuestion;
 }
@@ -74,9 +97,8 @@ function checkAnswer() {
   const inputValue = answerInput.value;
   answerInput.value = '';
   if (inputValue === nowQuestion) {
-    // 下一題
-    // 時間重設
-    
+    totalCorrect++;
+    totalTime += aWordUseTime;
     nextQuestion();
   }
 }
@@ -87,7 +109,7 @@ function getJson() {
     .then((output) => {
       jsonContent = output;
       setScore(output)
-      // console.log('Output: ', jsonContent);
+      startBtn.disabled = false;
     })
     .catch(err => console.error(err));
 }
@@ -95,21 +117,30 @@ function getJson() {
 function setScore(jsonRes) {
   let score = jsonRes.length;
   scoreSpan.innerHTML = score;
+  nowScore = score;
 }
 
 function resetGame() {
-
+  getJson();
+  totalTime = 0;
+  totalCorrect = 0;
+  isPlayingGame = false;
+  startBtn.innerHTML = '시작';
+  secondSpan.innerHTML = '?';
+  questionLabel.innerHTML = '문제 단어'
+  answerInput.value = '';
 }
 
 getJson();
 
-// function component() {
-//   let element = document.createElement('div');
+const restartBtn = document.querySelector('#restart-btn');
 
-//   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-//   element.innerHTML = _.join(['Hello', 'webpack222'], ' ');
+restartBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  startContainer.style.display = 'block';
+  finishContainer.style.display = 'none';
+  window.history.pushState(null, '타자게임', './index.html');
+  resetGame();
+})
 
-//   return element;
-// }
 
-// document.body.appendChild(component());
