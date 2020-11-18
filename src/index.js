032348bf7cfab598ae5213267fbb4b064e40e4c5
品君林 <jinglelin82@gmail.com>
2020-11-18 16:04:01 +0900

    實作開始畫面

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import _ from 'lodash';
+import './all.scss';
 
-function component() {
-  let element = document.createElement('div');
+let isPlayingGame = false;
+const startBtn = document.querySelector('#start-btn');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack222'], ' ');
+startBtn.addEventListener('click', () => {
+  startBtn.innerHTML = isPlayingGame? '시작' : '초기화';
+  isPlayingGame = !isPlayingGame;
+  console.log('isPlayingGame:', isPlayingGame);
+})
 
-  return element;
-}
 
-document.body.appendChild(component());
+// function component() {
+//   let element = document.createElement('div');
+
+//   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+//   element.innerHTML = _.join(['Hello', 'webpack222'], ' ');
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
